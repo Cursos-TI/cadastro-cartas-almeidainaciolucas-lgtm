c6a4f3a53bf538b01f11a58232393d5a91af0251
almeidainaciolucas-lgtm <almeidainaciolucas@gmail.com>
2026-06-08 00:06:49 +0000

    inserido as variaveis, falta os resultados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,34 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    int estado; //* letra que representa o estado, (EX: A a H.)
+    char codigo[5]; //* Código da carta (A01)
+    char nomeCidade[20]; //* nome da cidade.
+    int populacao; //* numero da população
+    float areakm; //* area em Km²
+    float pib; //* PIB
+    int pontosturisticos; //*número de pontos turisticos das cidades
 
   // Área para entrada de dados
+    printf("insira o estado: ");
+    scanf("%d", &estado);
+    printf("Insira o código: ");
+    scanf("%s", &codigo);
+    prinf("Insira o nome cidade: ");
+    scanf("%s", &nomeCidade);
+    printf("Insira população: ");
+    scanf("%d", &populacao);
+    printf("insira a áreaem Km²: ");
+    scanf("%f", &areakm);
+    prinf("insira PIB: ");
+    scanf("%f", &pib);
+    prinf("Número de pontos turisticos: ");
+    scanf("%d", &pontosturisticos);
 
   // Área para exibição dos dados da cidade
+    printf("Estado: %d\n", estado);
+
+
 
 return 0;
 } 
